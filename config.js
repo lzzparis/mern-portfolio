cd235301890250f86c32b2de41efd22306f91433
Lizzie Paris <lzz.paris@gmail.com>
2016-10-14 21:20:47 -0700

    Update production to mLab blog-post URL

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 exports.DATABASE_URL  =	process.env.DATABASE_URL ||
                         global.DATABASE_URL ||
                         (process.env.NODE_ENV === "production" ?
-                          "mongodb://insecure:2potatoes@ds041566.mlab.com:41566/mongoose-shopping-list" :
+                          "mongodb://insecure:2potatoes@ds013911.mlab.com:13911/blog-tool" :
                           "mongodb://localhost/");
 
 exports.PORT = process.env.PORT || 8080;
