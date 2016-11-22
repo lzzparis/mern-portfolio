ba5720be718e2c3e3b1e55db768293bdbec5180c
Lizzie Paris <lzz.paris@gmail.com>
2016-11-21 21:41:42 -0800

    fetchAllPosts when LatestPost mounts

--- client/js/components/latest-posts.js
+++ client/js/components/latest-posts.js
@@ -1,8 +1,12 @@
 var React = require("react");
 
+var actions = require("../actions/actions");
 var FullPost = require("./full-post");
 
 var LatestPosts = React.createClass({
+  componentWillMount: function() {
+    this.props.dispatch(actions.fetchAllPosts());
+  },
   render: function() {
     var list = [];
     for (var i=0 ; i < this.props.posts.length ; i++){
