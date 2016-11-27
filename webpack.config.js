c976baeb45b23487df62ca9b23ff87fbede58f84
Lizzie Paris <lzz.paris@gmail.com>
2016-11-26 16:55:30 -0800

    Production environment tweaks

--- webpack.config.js
+++ webpack.config.js
@@ -41,7 +41,7 @@ module.exports = {
         path: path.resolve(__dirname, outputPath),
         filename: filename,
     },
-    devtool: isProduction ? 'source-map' : 'inline-source-map',
+    devtool: 'inline-source-map',
     module: {
       loaders: [
         {
