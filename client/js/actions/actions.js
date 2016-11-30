7e7c8882c7817a4bcd2d4a6bc994f4b3cc34fa94
Lizzie Paris <lzz.paris@gmail.com>
2016-11-29 19:49:18 -0800

    Fix #27: remove markdown.toHtml and package. No need with ReactMarkdown

--- client/js/actions/actions.js
+++ client/js/actions/actions.js
@@ -1,7 +1,6 @@
 var fetch = require("isomorphic-fetch");
 var router = require("react-router");
 var hashHistory = router.hashHistory; 
-var markdown = require("markdown").markdown;
 
 
 var INIT_USER_SUCCESS = "INIT_USER_SUCCESS";
@@ -188,7 +187,6 @@ var createPost = function(post) {
 
 var FETCH_FULL_POST_DISPLAY = "FETCH_FULL_POST_DISPLAY";
 var fetchFullPostDisplay = function(post) {
-  post.body = markdown.toHTML(post.body);   
   return {
     type: FETCH_FULL_POST_DISPLAY,
     post: post
--- test/server/test-index.js
+++ test/server/test-index.js
@@ -63,7 +63,6 @@ describe('App name', function(){
         .get("/post/"+testId)
         .end(function(err,res){
           res.body.subject.should.equal("Biscuit recipe");
-// TODO - markdown          res.body.body.should.equal("<p>In a large bowl combine flour, sugar, baking powder and salt together. Cut butter into mixture until it begins to look like cornmeal. Make a well with flour mixture and slowly add milk into the middle. Knead dough with your fingers and add milk when necessary. Roll out dough onto a lightly floured surface and roll out to desired thickness. Cut with small biscuit cutter. Butter bottom of skillet and place biscuits in pan. Cover and place on top of hot coals in the fireplace. Carefully place some hot coals on top of the skillet cover. Bake for 12 minutes or until golden brown. Recipe courtesy of Paula Deen, 2008</p>");
           res.body.body.should.equal("In a large bowl combine flour, sugar, baking powder and salt together. Cut butter into mixture until it begins to look like cornmeal. Make a well with flour mixture and slowly add milk into the middle. Knead dough with your fingers and add milk when necessary. Roll out dough onto a lightly floured surface and roll out to desired thickness. Cut with small biscuit cutter. Butter bottom of skillet and place biscuits in pan. Cover and place on top of hot coals in the fireplace. Carefully place some hot coals on top of the skillet cover. Bake for 12 minutes or until golden brown. Recipe courtesy of Paula Deen, 2008");
           res.body.timestamp.should.not.equal("Thu Oct 06 2016 17:18:20 GMT-0700 (MST)");
           done();
@@ -84,7 +83,6 @@ describe('App name', function(){
         .get("/post/"+testId)
         .end(function(err,res){
           res.body.subject.should.equal("Biscuit recipe");
-// TODO - markdown          res.body.body.should.equal("<p>In a large bowl combine flour, sugar, baking powder and salt together. Cut butter into mixture until it begins to look like cornmeal. Make a well with flour mixture and slowly add milk into the middle. Knead dough with your fingers and add milk when necessary. Roll out dough onto a lightly floured surface and roll out to desired thickness. Cut with small biscuit cutter. Butter bottom of skillet and place biscuits in pan. Cover and place on top of hot coals in the fireplace. Carefully place some hot coals on top of the skillet cover. Bake for 12 minutes or until golden brown. Recipe courtesy of Paula Deen, 2008</p>");
           res.body.body.should.equal("In a large bowl combine flour, sugar, baking powder and salt together. Cut butter into mixture until it begins to look like cornmeal. Make a well with flour mixture and slowly add milk into the middle. Knead dough with your fingers and add milk when necessary. Roll out dough onto a lightly floured surface and roll out to desired thickness. Cut with small biscuit cutter. Butter bottom of skillet and place biscuits in pan. Cover and place on top of hot coals in the fireplace. Carefully place some hot coals on top of the skillet cover. Bake for 12 minutes or until golden brown. Recipe courtesy of Paula Deen, 2008");
           res.body.img.should.equal("http://thebarking.com/wp-content/uploads/2012/09/biscuit.jpg");
           res.body.timestamp.should.not.equal("Thu Oct 06 2016 17:18:20 GMT-0700 (MST)");
@@ -106,7 +104,6 @@ describe('App name', function(){
         .get("/post/"+testId)
         .end(function(err,res){
           res.body.subject.should.equal("Cutest kitty");
-// TODO - markdown          res.body.body.should.equal("<p>Look at this cat isn&#39;t it the cutest cat i love cats they are the best animals evar</p>");
           res.body.body.should.equal("Look at this cat isn't it the cutest cat i love cats they are the best animals evar");
           res.body.img.should.equal("");
           res.body.timestamp.should.not.equal("Thu Oct 06 2016 17:18:20 GMT-0700 (MST)");
