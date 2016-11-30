63ba2b676db6ca0be9af06691eed201dde2b05fc
Lizzie Paris <lzz.paris@gmail.com>
2016-11-29 23:11:33 -0800

    Style blog header, blog box-shadow

--- client/js/components/blog-nav.js
+++ client/js/components/blog-nav.js
@@ -6,10 +6,10 @@ var Link = router.Link;
 var BlogNav = function() {
   return (
     <ul className="blog-nav">
-      <li><Link className="blog-nav-link" to="/">Home</Link></li>
-      <li><Link className="blog-nav-link" to="/">About</Link></li>
-      <li><Link className="blog-nav-link" to="/">Portfolio</Link></li>
-      <li><Link className="blog-nav-link" to="/login">Login</Link></li>
+      <li className="blog-nav-item"><Link className="blog-nav-link" to="/">Home</Link></li>
+      <li className="blog-nav-item"><Link className="blog-nav-link" to="/">About</Link></li>
+      <li className="blog-nav-item"><Link className="blog-nav-link" to="/">Portfolio</Link></li>
+      <li className="blog-nav-item"><Link className="blog-nav-link" to="/login">Login</Link></li>
       <hr />
     </ul>
   );
