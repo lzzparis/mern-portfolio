270c2ab331ad9a3b8bb3b9dfc7a1f9dece395982
Lizzie Paris <lzz.paris@gmail.com>
2016-12-04 13:30:23 -0800

    Add hooks for react-slick eventually

--- client/js/components/project-list.js
+++ client/js/components/project-list.js
@@ -1,14 +1,17 @@
 var React = require("react");
+var Slider = require("react-slick");
 
 var Project = require("./project");
 
 var PROJECTS = [
   {name: "EtsyNearMe", description: "Etsy store locator", thumbnail: "http://placekitten.com/300/900"},
   {name: "Blog-Tool", description: "Simple Node/MongoDB blog tool ", thumbnail: "http://placekitten.com/800/200"},
-  {name: "Artist Hunter", description: "React Spotify", thumbnail: "http://placekitten.com/300/300"},
+  {name: "Artist Hunter", description: "React Spotify", thumbnail: "http://placekitten.com/400/400"},
   {name: "EtsyNearMe", description: "Full stack MERN blog tool", thumbnail: "http://placekitten.com/800/800"},
 
 ];
+
+
 
 var ProjectList = React.createClass({
   getInitialState: function() {
@@ -18,13 +21,30 @@ var ProjectList = React.createClass({
   },
   render: function() {
     var list = [];
-    for(var i=0; i < this.state.projects.length; i++) {
-      var currentProject = this.state.projects[i];
-      list.push(<Project key={i} name={currentProject.name} description={currentProject.description} thumbnail={currentProject.thumbnail} />);
+    if (!this.state.projects.length) {
+      list.push(<div></div>);
     }
+    else {
+      for (var i=0; i < this.state.projects.length; i++) {
+        var currentProject = this.state.projects[i];
+        list.push(<Project key={i} name={currentProject.name} description={currentProject.description} thumbnail={currentProject.thumbnail} />);
+      }
+    }
+    var settings = {
+      // infinite: false,
+      // speed: 500,
+      // slidesToShow: 1,
+      // slidesToScroll: 1
+      dots: true
+    };
+    // return (
+    //   <Slider className="project-list" {...settings}>
+    //     {list || <div></div>}
+    //   </Slider>
+    // );
     return (
       <div className="project-list">
-        {list}
+        {list || <div></div>}
       </div>
     );
   }
