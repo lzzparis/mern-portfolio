a9733d9e682af685fd186d24b988e261103bf4f8
Lizzie Paris <lzz.paris@gmail.com>
2016-12-05 23:27:37 -0800

    Update production DB with MLab blog-tool db

--- server/config.js
+++ server/config.js
@@ -1,7 +1,7 @@
 exports.DATABASE_URL  =	process.env.DATABASE_URL ||
                         global.DATABASE_URL ||
                         (process.env.NODE_ENV === "production" ?
-                          "mongodb://user:pass@machine.mlab.com:port/repo-name" :
+                          "mongodb://insecure:2potatoes@ds013911.mlab.com:13911/blog-tool" :
                           "mongodb://localhost/portfolio");
 
 exports.PORT = process.env.PORT || 8080;
