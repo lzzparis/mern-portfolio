0b1950edde43cde3b67117245e015e78e1dcdaa5
Lizzie Paris <lzz.paris@gmail.com>
2017-01-03 21:22:44 -0800

    Use sample-data.js POSTS for /post/all initialization; update dev DATABASE_URL

--- server/config.js
+++ server/config.js
@@ -2,7 +2,7 @@ exports.DATABASE_URL  =	process.env.DATABASE_URL ||
                         global.DATABASE_URL ||
                         (process.env.NODE_ENV === "production" ?
                           "mongodb://insecure:2potatoes@ds013911.mlab.com:13911/blog-tool" :
-                          "mongodb://localhost/");
+                          "mongodb://localhost/blog-tool-dev");
 
 exports.PORT = process.env.PORT || 8080;
 
