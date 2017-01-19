02e1e629949d3da5a38f87ccc06016771f2af5ec
Lizzie Paris <lzz.paris@gmail.com>
2017-01-19 15:16:49 -0800

    Add blog tests

--- test/client/test-components.js
+++ test/client/test-components.js
@@ -47,3 +47,132 @@ describe("ProjectList component", function() {
 	});
 });
 
+var POSTS = require("../sample-data");
+
+var Admin = require("../../client/js/components/admin");
+var AdminWrapper = shallow(<Admin  dispatch={dispatch} />);
+describe("(Component) Admin", function() {
+	it("renders", function() {
+		AdminWrapper.should.have.length(1);
+	});
+});
+
+
+var BlogApp = require("../../client/js/components/blog-app");
+var BlogAppWrapper = shallow(<BlogApp />);
+describe("(Component) BlogApp", function() {
+	it("renders", function() {
+		BlogAppWrapper.should.have.length(1);
+	});
+});
+
+
+var Blog = require("../../client/js/components/blog");
+var BlogWrapper = shallow(<Blog  dispatch={dispatch} />);
+describe("(Component) Blog", function() {
+	it("renders", function() {
+		BlogWrapper.should.have.length(1);
+	});
+});
+
+
+var BlogHeader = require("../../client/js/components/blog-header");
+var BlogHeaderWrapper = shallow(<BlogHeader />);
+describe("(Component) BlogHeader", function() {
+	it("renders", function() {
+		BlogHeaderWrapper.should.have.length(1);
+	});
+});
+
+
+var BlogMain = require("../../client/js/components/blog-main");
+var BlogMainWrapper = shallow(<BlogMain />);
+describe("(Component) BlogMain", function() {
+	it("renders", function() {
+		BlogMainWrapper.should.have.length(1);
+	});
+});
+
+
+var BlogNav = require("../../client/js/components/blog-nav");
+var BlogNavWrapper = shallow(<BlogNav />);
+describe("(Component) BlogNav", function() {
+	it("renders", function() {
+		BlogNavWrapper.should.have.length(1);
+	});
+});
+
+
+var FullPost = require("../../client/js/components/full-post");
+var FullPostWrapper = shallow(<FullPost dispatch={dispatch} post={POSTS[0]}/>);
+describe("(Component) FullPost", function() {
+	it("renders", function() {
+		FullPostWrapper.should.have.length(1);
+	});
+});
+
+
+var LatestPosts = require("../../client/js/components/latest-posts");
+var LatestPostsWrapper = shallow(<LatestPosts dispatch={dispatch} posts={POSTS}/>);
+describe("(Component) LatestPosts", function() {
+	it("renders", function() {
+		LatestPostsWrapper.should.have.length(1);
+	});
+});
+
+
+var Login = require("../../client/js/components/login");
+var LoginWrapper = shallow(<Login  dispatch={dispatch} />);
+describe("(Component) Login", function() {
+	it("renders", function() {
+		LoginWrapper.should.have.length(1);
+	});
+});
+
+
+var PostForm = require("../../client/js/components/post-form");
+var PostFormWrapper = shallow(<PostForm />);
+describe("(Component) PostForm", function() {
+	it("renders", function() {
+		PostFormWrapper.should.have.length(1);
+	});
+});
+
+
+var PostList = require("../../client/js/components/post-list");
+var PostListWrapper = shallow(<PostList dispatch={dispatch} posts={POSTS} />);
+describe("(Component) PostList", function() {
+	it("renders", function() {
+		PostListWrapper.should.have.length(1);
+	});
+});
+
+
+var PostSummary = require("../../client/js/components/post-summary");
+var PostSummaryWrapper = shallow(<PostSummary post={POSTS[0]} />);
+describe("(Component) PostSummary", function() {
+	it("renders", function() {
+		PostSummaryWrapper.should.have.length(1);
+	});
+});
+
+
+var PreviewPost = require("../../client/js/components/preview-post");
+var PreviewPostWrapper = shallow(<PreviewPost />);
+describe("(Component) PreviewPost", function() {
+	it("renders", function() {
+		PreviewPostWrapper.should.have.length(1);
+	});
+});
+
+
+var Sidebar = require("../../client/js/components/sidebar");
+var SidebarWrapper = shallow(<Sidebar />);
+describe("(Component) Sidebar", function() {
+	it("renders", function() {
+		SidebarWrapper.should.have.length(1);
+	});
+});
+
+
+
--- test/client/test-routes.js
+++ test/client/test-routes.js
@@ -43,4 +43,58 @@ describe("Routes", function(){
     })
     done();
   })
+
+/*Blog*/
+  it("/blog/posts/latest renders BlogApp, Blog, LatestPosts", function(done) {
+    match({routes, location: "/blog/posts/latest"}, function(error, redirectLocation, renderProps) {
+      var results = renderProps.components;      
+      results[1].displayName.should.equal("App");
+      results[2].WrappedComponent.displayName.should.equal("BlogApp");
+      results[3].WrappedComponent.displayName.should.equal("Blog");
+      results[4].WrappedComponent.displayName.should.equal("LatestPosts");
+    })
+    done();
+  })
+  it("/blog/posts/full/:id renders BlogApp, Blog, FullPost", function(done) {
+    match({routes, location: "/blog/posts/full/1000"}, function(error, redirectLocation, renderProps) {
+      var results = renderProps.components;      
+      results[1].displayName.should.equal("App");
+      results[2].WrappedComponent.displayName.should.equal("BlogApp");
+      results[3].WrappedComponent.displayName.should.equal("Blog");
+      results[4].WrappedComponent.displayName.should.equal("FullPost");
+    })
+    done();
+  })
+  it("/blog/login renders BlogApp, Login", function(done) {
+    match({routes, location: "/blog/login"}, function(error, redirectLocation, renderProps) {
+      var results = renderProps.components;      
+      results[1].displayName.should.equal("App");
+      results[2].WrappedComponent.displayName.should.equal("BlogApp");
+      results[3].WrappedComponent.displayName.should.equal("Login");
+    })
+    done();
+  })
+  it("/blog/admin renders BlogApp, Admin", function(done) {
+    match({routes, location: "/blog/admin"}, function(error, redirectLocation, renderProps) {
+      var results = renderProps.components;      
+      results[1].displayName.should.equal("App");
+      results[2].WrappedComponent.displayName.should.equal("BlogApp");
+      results[3].WrappedComponent.displayName.should.equal("Admin");
+    })
+    done();
+  })
+  it("/blog/admin/preview/:id renders BlogApp, Admin, PreviewPost", function(done) {
+    match({routes, location: "/blog/admin/preview/1000"}, function(error, redirectLocation, renderProps) {
+      var results = renderProps.components;      
+      results[1].displayName.should.equal("App");
+      results[2].WrappedComponent.displayName.should.equal("BlogApp");
+      results[3].WrappedComponent.displayName.should.equal("Admin");
+      results[4].displayName.should.equal("PreviewPost");
+      results[5].WrappedComponent.displayName.should.equal("FullPost");
+    })
+    done();
+  })
+
+
+
 }); 
