3a38359c45fed5e38729634da5b3f1de570a9985
Lizzie Paris <lzz.paris@gmail.com>
2017-01-18 22:21:05 -0800

    Tweak style for cohesion - still needs cleanup

--- client/js/components/connect.js
+++ client/js/components/connect.js
@@ -7,18 +7,18 @@ var Connect = React.createClass({
       <div className="connect">
         <h2 className="connect-header">Get in Touch</h2>
         <ul className="connect-list">
-          <li className="left connect-list-item">
+          <li className="left text-left connect-list-item">
             <a className="connect-list-item-link hover-bottom" href="https://github.com/lzzparis" target="_blank">Github</a>
           </li>
-          <li className="right connect-list-item">
+          <li className="right text-right connect-list-item">
             <a className="connect-list-item-link hover-bottom" href="https://www.linkedin.com/in/lzzparis" target="_blank">LinkedIn</a>
           </li>
         </ul>
         <ul className="connect-list">
-          <li className="left connect-list-item">
+          <li className="left text-left connect-list-item">
             <a className="connect-list-item-link hover-top" href="mailto:lzz.paris@gmail.com" target="_blank">Email</a>
           </li>
-          <li className="right connect-list-item">
+          <li className="right text-right connect-list-item">
             <a className="connect-list-item-link hover-top" href="https://www.instagram.com/lzzparis/" target="_blank">Instagram</a>
           </li>
         </ul>
