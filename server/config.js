312cb2844b18af3cd2b76a73fa13dd759ef84d6f
Lizzie Paris <lzz.paris@gmail.com>
2017-01-18 16:57:02 -0800

    Move some unused files out of the way to prep for blog integ

--- server/config.js
+++
@@ -1,8 +0,0 @@
-exports.DATABASE_URL  =	process.env.DATABASE_URL ||
-                        global.DATABASE_URL ||
-                        (process.env.NODE_ENV === "production" ?
-                          "mongodb://insecure:2potatoes@ds013911.mlab.com:13911/blog-tool" :
-                          "mongodb://localhost/portfolio");
-
-exports.PORT = process.env.PORT || 8080;
-
