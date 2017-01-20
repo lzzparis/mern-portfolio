849e22774fe410091fdb72614cdc1e4eb6d1bef5
Lizzie Paris <lzz.paris@gmail.com>
2017-01-19 16:51:22 -0800

    Remove server/config.js from Git control for database security

--- server/config.js
+++
@@ -1,9 +0,0 @@
-exports.DATABASE_URL  =	process.env.DATABASE_URL ||
-                        global.DATABASE_URL ||
-                        (process.env.NODE_ENV === "production" ?
-                          "mongodb://insecure:2potatoes@ds013911.mlab.com:13911/blog-tool" :
-                          "mongodb://localhost/blog-tool-dev");
-
-exports.PORT = process.env.PORT || 8080;
-
-
