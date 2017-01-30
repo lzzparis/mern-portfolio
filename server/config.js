b2910bd01e9968477ea334899a717006ff449c3f
Lizzie Paris <lzz.paris@gmail.com>
2017-01-30 14:48:45 -0800

    Flip marble, reinstate port 8080

--- server/config.js
+++ server/config.js
@@ -4,6 +4,6 @@ exports.DATABASE_URL  =	process.env.DATABASE_URL ||
                           "mongodb://insecure:2potatoes@ds013911.mlab.com:13911/blog-tool" :
                           "mongodb://localhost/blog-tool-dev");
 
-exports.PORT = process.env.PORT || 80;
+exports.PORT = process.env.PORT || 8080;
 
 
