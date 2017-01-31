a1c17c3b5f7633f6da80603c1eb4e94b9005ddaa
Lizzie Paris <lzz.paris@gmail.com>
2017-01-31 10:49:30 -0800

    Rearrange projects into grid

--- client/js/components/project.js
+++ client/js/components/project.js
@@ -1,17 +1,12 @@
 var React = require("react");
 
 var Project =  function(props) {
-  var sideClass = ""; 
-  if(props.setRight) { sideClass = "project-right"; }
-  else { sideClass = "project-left"; }
   return (
-    <div className={"project "+sideClass}>
+    <div className={"project"}>
       <a href={props.url} target="_blank">
         <img className="project-thumbnail" src={props.thumbnail} />
-        <div className="project-content" href={props.url}>
           <h2 className="project-content-name">{props.name}</h2>
           <p className="project-content-description">{props.description}</p>
-        </div>
       </a>
     </div>
   );
